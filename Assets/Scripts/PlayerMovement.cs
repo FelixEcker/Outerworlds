999768c4087843f8020fc2870e866b9577fc3314
Bertrahm <felix_eckert_@t-online.de>
2020-03-09 19:38:54 +0100

    Added test scene, movement script and terrain generation test.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿/****************************************/
+/*Outerworlds Player Movement Controller*/
+/*Author: Felix Eckert                  */
+/****************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Speed setting variables
+    public float maxSpeed;
+    //////////////////////////
+
+    // Objects to be moved
+    public Rigidbody2D player; // Player's Rigidbody
+    public Camera camera;      // Camera
+    //////////////////////
+
+    public Vector3 cameraOffset; // The cameras offset relative to the player
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical   = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        player.velocity = movement * maxSpeed;
+
+        Vector3 newCameraPosition = new Vector3(player.position.x + cameraOffset.x, player.position.y + cameraOffset.y, cameraOffset.z);
+        camera.transform.position = newCameraPosition;
+    }
+}
